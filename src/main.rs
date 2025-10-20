023e6dd424455b520689da58a4020773b29fa6c7
zfphex <28522740+zX3no@users.noreply.github.com>
2025-10-20 13:26:03 +1030

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 use macwalk::*;
 
 pub fn main() {
@@ -5,6 +6,12 @@ pub fn main() {
     let path_str = if args.len() > 1 { &args[1] } else { "." };
 
     println!("Listing contents of directory: \"{}\"", path_str);
+
+    unsafe {
+        recursive_walk(path_str, 0);
+    }
+
+    return;
 
     unsafe {
         let ns_path_string = NSString(path_str);
@@ -21,4 +28,46 @@ pub fn main() {
             println!("- {}", item);
         }
     }
+}
+
+pub unsafe fn recursive_walk(path: &str, depth: usize) {
+    let ns_path_string = NSString(path);
+    let file_manager = NSFileManager::default();
+
+    let directory = file_manager.contentsOfDirectory(ns_path_string, core::ptr::null_mut());
+    if directory.is_null() {
+        return;
+    }
+
+    let len = count(directory);
+    for i in 0..len {
+        let id = objectAtIndex(directory, i);
+        let item = UTF8String(id);
+
+        // Print with indentation based on depth
+        for _ in 0..depth {
+            print!("  ");
+        }
+        println!("- {}", item);
+
+        // Build the full path and recursively traverse if it's a directory
+        let full_path = format!("{}/{}", path, item);
+
+        // You may want to add a check to see if it's actually a directory
+        // to avoid trying to traverse files
+        if is_directory(&full_path) {
+            recursive_walk(&full_path, depth + 1);
+        }
+    }
+}
+
+// Helper function to check if a path is a directory
+pub unsafe fn is_directory(path: &str) -> bool {
+    let ns_path = NSString(path);
+    let file_manager = NSFileManager::default();
+
+    // You'd need to add a method to check file attributes
+    // For now, you could try calling contentsOfDirectory and checking if it's null
+    let contents = file_manager.contentsOfDirectory(ns_path, core::ptr::null_mut());
+    !contents.is_null()
 }
